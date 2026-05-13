9fb13f92040a518c6113ca71b53667ad76bff1c3
samuelsrb <ssrbarroso@gmail.com>
2026-05-13 16:13:52 -0300

    feat: enable CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3333;
 
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
