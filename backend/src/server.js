0f29122a08f34d629a254b7450a915b77416c90a
samuelsrb <ssrbarroso@gmail.com>
2026-05-13 16:16:31 -0300

    feat: register task routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 require('./database');
 
+const tasksRoutes = require('./routes/tasks');
+
 const app = express();
 const port = process.env.PORT || 3333;
 
@@ -12,6 +14,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'API de tarefas funcionando.' });
 });
+
+app.use('/tasks', tasksRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
